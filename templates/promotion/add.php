8362e7c7ea7fc6473ed7104ce70b04a4800d75e7
adrienb39 <adrienb39@yahoo.com>
2024-12-16 11:25:30 +0100

    ajout de l'année actuelle dans le value du formulaire de la création d'une promotion

--- templates/promotion/add.php
+++ templates/promotion/add.php
@@ -9,7 +9,10 @@
                 </div>
                 <div class="mb-3">
                     <label for="annee_promotion" class="form-label">Année</label>
-                    <input type="text" class="form-control" id="annee_promotion" name="annee_promotion" value="<?= $anneePromotion; ?>" required>
+                    <input type="number" class="form-control" minlength="4" maxlength="4" id="annee_promotion" name="annee_promotion" value="<?php if (!isset($anneePromotion)) { $anneePromotion; } else {
+                        $nowYear = new \DateTime();
+                        echo $NowYear = $nowYear->format('Y');
+                    } ?>" required>
                 </div>
                 <button type="submit" class="btn btn-primary">Créer la promotion</button>
             </form>
